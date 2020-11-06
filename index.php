9bdf6686e360709c25d9b985af38d340ed2a4027
nheudron <Secure_150>
2020-11-06 02:33:47 +0100

    style

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 		<link rel="stylesheet" href="index.css" />
 		<link rel="icon" href="images/favicon.png" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
 	</head>
 	<body>
 		<?php
